a9c5d2d77ad60742ad8bf878f174c2954d8ba76a
Tishu100 <pickme10030@gmail.com>
2021-05-30 23:30:51 +0900

    2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,12 +3,15 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\Form\Forms;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    $formFactory = Forms::createFormFactory();
+    $formFactory = Forms::createFormFactoryBuilder()
+        ->addExtension(new HttpFoundationExtension())
+        ->getFormFactory();
     /** 
      * @Route("home/index", name="homepage")
     */
